d1ceceeb48efe22acd02efecbab61739ded2f89f
nadir566 <nadir2009zeyd@gmail.com>
2025-05-26 13:56:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit,QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
-
+#да
 import json
 
 app = QApplication([])
